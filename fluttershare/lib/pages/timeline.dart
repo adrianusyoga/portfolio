2c12f7f757e13405b0ad1a352b5f3d738801da8b
Adrianus Yoga <adrianusyoga@yahoo.com>
2020-02-12 02:41:26 +0700

    firebase DB

--- fluttershare/lib/pages/timeline.dart
+++ fluttershare/lib/pages/timeline.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/widgets/header.dart';
 import 'package:fluttershare/widgets/progress.dart';
+
+final usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -8,6 +11,20 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  @override
+  void initState() {
+    getUsers();
+    super.initState();
+  }
+
+  getUsers() {
+    usersRef.getDocuments().then((QuerySnapshot snapshot) {
+      snapshot.documents.forEach((DocumentSnapshot doc) {
+        print(doc.data);
+      });
+    });
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
